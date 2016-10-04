b68c867d489f1850b813f26a16606dabea4920e0
Cyril Margorin <cyril@imperx.com>
2016-10-04 12:38:28 +0300

    Fix deploy/debug targets

--- cmake/FindOpenOCD.cmake
+++ cmake/FindOpenOCD.cmake
@@ -12,25 +12,43 @@ find_package_handle_standard_args(OpenOCD
 
 set(gdbinit_file ${CMAKE_CURRENT_LIST_DIR}/gdbinit)
 function(enable_deploy TARGET)
-	add_custom_command(
-		TARGET ${TARGET} POST_BUILD
-		COMMENT "Deploying to board"
-		COMMAND ${OpenOCD_EXE} ARGS -f board/st_nucleo_f4.cfg -c \" program $<TARGET_FILE:${TARGET}> verify reset exit \"
-	)
+	get_property(TARGET_DEPLOYED TARGET ${TARGET} PROPERTY TARGET_DEPLOYED)
+	if(TARGET_DEPLOYED)
+		message(FATAL_ERROR "Deployment already specifies")
+	else()
+		add_custom_command(
+			TARGET ${TARGET} POST_BUILD
+			COMMENT "Deploying to board"
+			COMMAND ${OpenOCD_EXE} ARGS -f board/st_nucleo_f4.cfg -c \" program $<TARGET_FILE:${TARGET}> verify reset exit \"
+		)
+		set_property(TARGET ${TARGET} PROPERTY TARGET_DEPLOYED ${TARGET})
+	endif(TARGET_DEPLOYED)
 endfunction(enable_deploy)
 
 function(add_deploy_target TARGET)
-	add_custom_target(deploy_${TARGET}
-		COMMENT "Deploying to board"
-		COMMAND ${OpenOCD_EXE} ARGS -f board/st_nucleo_f4.cfg -c \" program $<TARGET_FILE:${TARGET}> verify reset exit \"
-	)
-	add_dependencies(deploy_${TARGET} ${TARGET})
+	get_property(TARGET_DEPLOYED TARGET ${TARGET} PROPERTY TARGET_DEPLOYED)
+	if(TARGET_DEPLOYED)
+		message(FATAL_ERROR "Deployment already specifies")
+	else()
+		add_custom_target(deploy_${TARGET}
+			COMMENT "Deploying to board"
+			COMMAND ${OpenOCD_EXE} ARGS -f board/st_nucleo_f4.cfg -c \" program $<TARGET_FILE:${TARGET}> verify reset exit \"
+		)
+		add_dependencies(deploy_${TARGET} ${TARGET})
+		set_property(TARGET ${TARGET} PROPERTY TARGET_DEPLOYED deploy_${TARGET})
+	endif(TARGET_DEPLOYED)
 endfunction(add_deploy_target)
 
 function(add_debug_target TARGET)
+	get_property(TARGET_DEPLOYED TARGET ${TARGET} PROPERTY TARGET_DEPLOYED)
+	if(NOT TARGET_DEPLOYED)
+		add_deploy_target(${TARGET})
+		get_property(TARGET_DEPLOYED TARGET ${TARGET} PROPERTY TARGET_DEPLOYED)
+	endif()
+
 	add_custom_target(debug_${TARGET}
 		COMMENT "Running debugger"
 		COMMAND arm-none-eabi-gdb -ex \" target remote | ${OpenOCD_EXE} -f board/st_nucleo_f4.cfg -c \\\"gdb_port pipe\\\" \" -x ${gdbinit_file} $<TARGET_FILE:${TARGET}>
 	)
-	add_dependencies(debug_${TARGET} deploy_${TARGET})
+	add_dependencies(debug_${TARGET} ${TARGET_DEPLOYED})
 endfunction(add_debug_target)
